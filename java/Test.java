08e8f617b4da64987398b9caf5d9d3fc468137ce
lanshuqian <1051614108@qq.com>
2023-09-20 15:19:27 +0800

    第一次idea提交代码

---
+++ java/Test.java
@@ -0,0 +1,15 @@
+package java;
+
+/**
+ * @CreateDate: 2023-09-20 15:03
+ * @Author: hspcadmin
+ * <p>
+ * Copyright © 2023 Hundsun Technologies Inc. All Rights Reserved
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("寄");
+        System.out.println(111);
+        System.out.println(222);
+    }
+}
